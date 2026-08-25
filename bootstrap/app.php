daf6ce1886e8c1ed69bd0537481a53028bdd40d7
Yoga Wilanda <hyoga.wilanda@gmail.com>
2026-08-25 15:00:25 +0700

    fix: proxies allowance for hosting

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        // untuk keperluan hosting
+         $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
